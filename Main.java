806bd06834e7922debc76bbd38d3e7fa79f14bf0
Aakash Rawat <01tripster@gmail.com>
2022-06-17 19:52:25 +0530

    added first java file to git

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world, Ash this side");
+        System.out.println("another line with the help of SOUT shortcut");
+
+//        for classes, we use PascalConvention
+//        for functions we use camelCaseConvention
+//        java is case-sensitive and can store different values in same type of word like value and VALUE are totally different variables.
+//        wide space in variable names is not allowed (use camelcase or pascal-case in large words name)
+//        don't start name of variable with numbers
+
+//        https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html#:~:text=32%2Dbit%20IEEE%20754%20floating%20point.-,Its%20range%20of%20values%20is%20beyond%20the%20scope%20of%20this%20discussion,-%2C%20but%20is%20specified%20in%20the%20Floating
+
+//        quick quiz ( write a java program to add 3 numbers )
+
+        int a = 3;
+        int b = 4;
+        int c = 5;
+        int sum = a+b+c;
+        System.out.print("the sum 3 numbers is:- ");
+        System.out.println(sum);
+    }
+}
