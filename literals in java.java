dc1310e8aed0817a6539bffd1cf91971ab4a2833
Aakash Rawat <01tripster@gmail.com>
2022-06-18 17:57:40 +0530

    literals in java

---
+++ literals in java.java
@@ -0,0 +1,22 @@
+// Literals in java.
+// in order to choose the data type we first need to find the type of data we want to store.
+// After that we need ot analyze the min and max value we might use.
+
+
+//Literals :- A constant value which can be assigned to the variable is called as a literal.
+
+public class cwh_04_literals {
+    public static void main(String[] args) {
+        byte age = 18;
+        int age2 = 56;
+        short age3 = 87;
+        long ageDino = 5666666666666L;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.66;
+        boolean a = true;
+        System.out.print(age);
+        String str = " Ash";
+        System.out.println(str);
+    }
+}
