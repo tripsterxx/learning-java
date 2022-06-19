5040f5d740940f2d5dda31211dbe1908e18cf531
Aakash Rawat <01tripster@gmail.com>
2022-06-20 03:25:28 +0530

    java chapter 2 practice set

---
+++ ps2_pr01.java
@@ -0,0 +1,7 @@
+public class cwh_12_ps2_pr01 {
+    public static void main(String[] args) {
+//        float a = 7/4 * 9/2; // ans --> 4.0
+        float a = 7/4.0f * 9/2.0f; // ans --> 7.875
+        System.out.println(a);
+    }
+}
---
+++ ps2_pr03and4.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class cwh_12_ps2_pr03and4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(num>8);
+
+        System.out.println(7*49/7+35/7);  // --> 54 ans
+    }
+}
