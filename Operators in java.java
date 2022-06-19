009623052d5dda26f9e92bc1a0d663ec74154f0b
Aakash Rawat <01tripster@gmail.com>
2022-06-19 20:48:20 +0530

    operators in java

--- Operators in java.java
+++ Operators in java.java
@@ -3,10 +3,10 @@ public class CWH_Ch2_Operators {
         //Arithmetic Operator
         //arithmetic operators does not work with booleans pretty obvious.
         int a = 4;
-//        int b = 6 + a;
-//        int b = 6 - a;
-//        int b = 6 * a;
-//        int b = 6 % a; // modulo operator__ gives remainder
+        int b = 6 + a;
+        int b = 6 - a;
+        int b = 6 * a;
+        int b = 6 % a; // modulo operator__ gives remainder
         //4.8%1.1 --> Returns Decimal Remainder.
 
         //Assignment Operator
@@ -15,12 +15,12 @@ public class CWH_Ch2_Operators {
         System.out.println(b);
 
         //Comparison operators
-//        System.out.println(6==8);// false
-//        System.out.println(6==6);// true
-//        System.out.println(6<10);//true and so on >=, <=, ==
+        System.out.println(6==8);// false
+        System.out.println(6==6);// true
+        System.out.println(6<10);//true and so on >=, <=, ==
 
         //Logical operator
-//        System.out.println(64>5 && 64>10); // both have to be true to give the output as true.
-//        System.out.println(65>5 || 65>80);// only one of them have to be true
+        System.out.println(64>5 && 64>10); // both have to be true to give the output as true.
+        System.out.println(65>5 || 65>80);// only one of them have to be true
     }
 }
