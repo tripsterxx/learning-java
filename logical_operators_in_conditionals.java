727218d1db5a5a57dccb63f233f9c2b9e892c252
Aakash Rawat <01tripster@gmail.com>
2022-06-22 02:07:53 +0530

    logical_operators_in_conditionals in java

---
+++ logical_operators_in_conditionals.java
@@ -0,0 +1,38 @@
+public class cwh_17_logical_operators_in_conditionals {
+    public static void main(String[] args) {
+        /*
+        System.out.println("For logical AND..");
+        boolean a = true;
+        boolean b = true;
+        if (a && b){
+            System.out.println("Y");
+        }
+        else {
+            System.out.println("N");
+        }
+
+         */
+
+        /*
+        System.out.println("For logical OR..");
+        boolean a = true;
+        boolean b = false;
+        if (a || b){
+            System.out.println("Y");
+        }
+        else {
+            System.out.println("N");
+        }
+
+         */
+
+        boolean a = true;
+        boolean b = false;
+        System.out.println("For logical NOT");
+        System.out.println("Not(a) is ");
+        System.out.println(!a);
+        System.out.println("Not(b) is ");
+        System.out.println(!b);
+
+    }
+}
