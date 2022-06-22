ebe02e7c1d2b8e21a945441268ccad2486bf6c92
Aakash Rawat <01tripster@gmail.com>
2022-06-23 02:53:54 +0530

    do_while_loop in java

---
+++ do_while_loop.java
@@ -0,0 +1,15 @@
+public class cwh_22_ch4_do_while {
+    public static void main(String[] args) {
+//        int a = 5;
+//        while(a<5){
+//            System.out.println(a);
+//            a++;
+//        }
+
+        int b = 18;
+        do {
+            System.out.println(b);
+            b++;
+        }while (b<5);
+    }
+}
