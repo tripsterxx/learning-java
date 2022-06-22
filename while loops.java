0d12c16f7499e92e5675fa1ce2b8a78d4f6b0a7e
Aakash Rawat <01tripster@gmail.com>
2022-06-23 00:56:37 +0530

    while loops in java

---
+++ while loops.java
@@ -0,0 +1,12 @@
+public class cwh_21__ch5_loops {
+    public static void main(String[] args) {
+
+        //Using loops
+        int i = 100;
+        while (i<=200){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Finish running while loop.");
+    }
+}
