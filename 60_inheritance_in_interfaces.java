2619f73b41dfa169728c87fdeb8fe3cf06c39f98
Aakash Rawat <01tripster@gmail.com>
2022-07-03 19:58:52 +0530

    inheritance_in_interfaces

---
+++ 60_inheritance_in_interfaces.java
@@ -0,0 +1,36 @@
+package com.company;
+
+interface sampleInterface{
+    void meth1();
+    void meth2();
+}
+
+interface childSampleInterface extends sampleInterface{
+    void meth3();
+    void meth4();
+}
+
+class MySampleClass implements childSampleInterface{
+    public void meth1(){
+        System.out.println("meth 1");
+    }
+    public void meth2(){
+        System.out.println("meth 2");
+    }
+    public void meth3(){
+        System.out.println("meth 3");
+    }
+    public void meth4(){
+        System.out.println("meth 4");
+    }
+}
+
+public class cwh_60_inheritance_in_interfaces {
+    public static void main(String[] args) {
+        MySampleClass obj = new MySampleClass();
+        obj.meth1();
+        obj.meth2();
+        obj.meth3();
+        obj.meth4();
+    }
+}
