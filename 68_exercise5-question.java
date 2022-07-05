d9f958b8f6bc41b26fa669aae7ba25b87131cdba
Aakash Rawat <01tripster@gmail.com>
2022-07-06 01:08:20 +0530

    68_exercise5-question

---
+++ 68_exercise5-question.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/*
+you have to create a package named com.codewithharry.shape
+this package should have individual classes for ractangle, square, circle, cylinder, sphere
+these classes should use inheritance to properly manage the code!
+include methods like volume, surface area and getters/setters for dimensions.
+ */
+
+public class cwh_68_exercise5 {
+}
