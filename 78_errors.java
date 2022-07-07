1009374f110ef5c1fa02d9f87bb8713eb8d9898a
Aakash Rawat <01tripster@gmail.com>
2022-07-08 01:57:07 +0530

    errors(1) in java

---
+++ 78_errors.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class cwh_78_errors {
+    public static void main(String[] args) {
+//        syntax error and logical error
+
+//        int a = 5
+//        int b = 4;
+//        System.out.println(a+b);
+    }
+}
