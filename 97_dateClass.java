130f47cad209c60e4b7ba2e09ed69a57b7f20acf
Aakash Rawat <01tripster@gmail.com>
2022-07-13 00:30:41 +0530

    date class in java

---
+++ 97_dateClass.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Date;
+
+public class cwh_97_dateClass {
+    public static void main(String[] args) {
+//        System.out.println(Long.MAX_VALUE);
+//        System.out.println(System.currentTimeMillis());
+        Date d = new Date();
+        System.out.println(d);
+        System.out.println(d.getTime());
+
+        //below 3 are deprecated
+        System.out.println(d.getDate());
+        System.out.println(d.getSeconds());
+        System.out.println(d.getYear());
+    }
+}
