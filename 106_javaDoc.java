fc273940de495e1b3240997071f1fdac4c7fa7e2
Aakash Rawat <01tripster@gmail.com>
2022-07-14 20:55:26 +0530

    java doc

---
+++ 106_javaDoc.java
@@ -0,0 +1,21 @@
+package com.company;
+
+/**
+ * This class is used to demonstrate what javadoc is and how it is used in the java industry.
+ * This is <i>italic</i> word. <p>This is a new paragraph</p>
+ * @author Ash (Aakash rawat)
+ * @version 0.1
+ * @since 2022
+ * @see <a href="https://docs.oracle.com/en/java/javase/17/docs/api/index.html" target="_blank">Java Docs</a>
+ */
+
+public class cwh_106_javaDoc {
+
+    public void add(int a, int b){
+        System.out.println("The sum is: " + a+b);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("This is my main method");
+    }
+}
